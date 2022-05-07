faae2fd011a70184dd49ef443b3faeda6acdc4ec
Aurora Borghi <aborghi@localhost.localdomain>
2022-05-07 15:07:02 -0600

    Initialized Python Backend.

---
+++ backend/proof/__init__.py
@@ -0,0 +1,8 @@
+def say_hello(name: str) -> None:
+    """
+    Says hello to the specified user.
+
+    :param name: Contains the name of the user.
+    """
+
+    print('Hello, ' + name)
---
+++ backend/test/test_placeholder.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestPlaceholder(unittest.TestCase):
+    """
+    Placeholder for future unit tests.
+    """
+
+    def test_placeholder(self) -> None:
+        """
+        Ensure 1 + 1 = 2.
+        """
+        self.assertEqual(1 + 1, 2)
